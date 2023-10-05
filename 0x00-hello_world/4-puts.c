dbb37d57bac3735e666e7f62e6845a2b7ed82110
blindlit0 <blindlit0@gmail.com>
2023-10-05 11:53:25 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+
+/**
+ * main - prints specified text
+ *
+ * Return: 0
+ *
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");;;;;;;;
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sizes of various types on the computer it is compiled and run on
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
