04eb00c0e65d28356d399f9116fae0982bb6f568
blindlit0 <blindlit0@gmail.com>
2023-10-12 17:25:24 -0700

    g

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *mul - multiply two numbers
+ *@a: first  number to be multiplied
+ *@b: number to be multiplied against first
+ *Return: the product of first and second number
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
