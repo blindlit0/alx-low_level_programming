e0514b9190107837bceab36e9bf9e28efa3d2c08
blindlit0 <blindlit0@gmail.com>
2023-10-16 09:46:51 -0700

    0-3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the length of a string and returns value
+ * @s - string
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+{
+	int string;
+
+	for (string = 0; s[string] != '\0' ; string++)
+
+	return (string);
+}	
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints astring followed by anew line
+ * @*str - string
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+	char string[];
+
+	string = *str;
+
+	return(string);
+}
+
+
+
