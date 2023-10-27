5a9be2f545c3342e32aa7cf72616e372b7ca5ee8
blindlit0 <blindlit0@gmail.com>
2023-10-27 11:37:00 -0700

    0

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: first char
+ * @c: second char
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies specific string to specific place
+ * @dest: where to copy string to
+ * @src: where to copy string from
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
