ffc410ab0cbb1d0200da30bc72fbd60334bfe991
blindlit0 <blindlit0@gmail.com>
2023-11-17 09:32:25 -0800

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,6 +8,7 @@
  * @owner: pointer to owner of dog
  * Return: void
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 		if (d)
@@ -15,5 +16,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-												}
+		}
 }
