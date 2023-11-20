9963bf9dde531ffdfa3090b96de62ca33c0eba0a
blindlit0 <blindlit0@gmail.com>
2023-11-20 14:01:21 -0800

    done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that prints the
+ * number of elements in a linked listint_t list.
+ * @h: head pointer
+ * Return: the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t list = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		list++;
+	}
+	return (list);
+}
